dd428d926694bcd632540546f80dc48320fe35ec
Alex Bogomol <alexander.bogomol@gmail.com>
2019-12-29 22:14:45 +0200

    Added Core project

---
+++ ContosoUniversity.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContosoUniversity.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
