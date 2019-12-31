6b17bd571824b3083916bddaf79ae97956b181f4
Alex Bogomol <alexander.bogomol@gmail.com>
2019-12-31 02:50:02 +0200

    Update solution

--- ContosoUniversity.Core/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ContosoUniversity.Core.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
