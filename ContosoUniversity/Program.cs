a6fc3d8b8e6085c449acbebe12701afd58c5e6bf
Alex Bogomol <alexander.bogomol@gmail.com>
2020-02-22 21:59:51 +0200

    Ensure database status

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using ContosoUniversity.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -13,13 +15,36 @@ namespace ContosoUniversity
         {
             var host = CreateWebHostBuilder(args).Build();
 
+            EnsureDatabaseFor(host);
+
+            host.Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+        /// <summary>
+        /// Ensure the database status for the host (created/migrated/initialized)
+        /// https://stackoverflow.com/a/55971168
+        /// https://docs.microsoft.com/en-us/ef/core/managing-schemas/ensure-created
+        /// https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli#apply-migrations-at-runtime
+        /// </summary>
+        private static void EnsureDatabaseFor(IWebHost host)
+        {
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
                 {
                     var context = services.GetRequiredService<SchoolContext>();
-                    DbInitializer.Initialize(context);
+
+                    if (context.Database.GetPendingMigrations().Any())
+                    {
+                        context.Database.Migrate();
+                    }
+
+                    DbInitializer.EnsureInitialized(context);
                 }
                 catch (Exception ex)
                 {
@@ -27,12 +52,6 @@ namespace ContosoUniversity
                     logger.LogError(ex, "An error occurred while seeding the database.");
                 }
             }
-
-            host.Run();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
