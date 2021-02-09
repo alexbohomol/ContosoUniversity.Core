5c3fe054c3af5995f1701184bc742094ea98c4e9
Alex Bogomol <alexander.bogomol@gmail.com>
2021-02-09 17:50:51 +0200

    Decouple Student entity from views

---
+++ ContosoUniversity/ViewModels/Students/EnrollmentViewModel.cs
@@ -0,0 +1,12 @@
+namespace ContosoUniversity.ViewModels.Students
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class EnrollmentViewModel
+    {
+        [Display(Name = "Course")]
+        public string CourseTitle { get; init; }
+
+        public string Grade { get; init; }
+    }
+}
--- ContosoUniversity/ViewModels/Students/StudentEditForm.cs
+++ ContosoUniversity/ViewModels/Students/StudentEditForm.cs
@@ -1,19 +1,14 @@
-﻿namespace ContosoUniversity.Models
+namespace ContosoUniversity.ViewModels.Students
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
-    public class Student : IExternalIdentifier
+    public class StudentEditForm
     {
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-
-        public ICollection<Enrollment> Enrollments { get; set; }
-        public int Id { get; set; }
 
         [Required]
         [StringLength(50)]
@@ -22,12 +17,8 @@
 
         [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
         [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName => LastName + ", " + FirstMidName;
+        public string FirstName { get; set; }
 
         public Guid ExternalId { get; set; }
     }
