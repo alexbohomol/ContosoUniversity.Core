edbb66fa280aa490c49c140e42ef1ba0676ebefe
Alex Bogomol <alexander.bogomol@gmail.com>
2021-02-15 14:44:03 +0200

    Enable migrations for each context

---
+++ ContosoUniversity.Data.Courses/Migrations/20210215113919_InitCoursesContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Data.Courses.Migrations
+{
+    public partial class InitCoursesContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "crs");
+
+            migrationBuilder.CreateTable(
+                name: "Course",
+                schema: "crs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseCode = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Credits = table.Column<int>(type: "int", nullable: false),
+                    DepartmentExternalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExternalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course",
+                schema: "crs");
+        }
+    }
+}
