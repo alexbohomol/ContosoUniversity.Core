a3eda5a46a2e5e33624984223bff647c9cd37511
Alex Bogomol <alexander.bogomol@gmail.com>
2021-04-14 03:25:30 +0300

    Pull StudentsRepository to base with missing Enrollments relation

--- ContosoUniversity.Data.Students/StudentsRepository.cs
+++ ContosoUniversity.Data.Students/StudentsRepository.cs
@@ -6,79 +6,19 @@ namespace ContosoUniversity.Data.Students
     using System.Threading.Tasks;
 
     using Domain.Contracts;
-    using Domain.Contracts.Exceptions;
     using Domain.Student;
 
     using Microsoft.EntityFrameworkCore;
 
-    public class StudentsRepository : IStudentsRepository
+    public class StudentsRepository : EfRepository<Student, Models.Student>, IStudentsRepository
     {
-        private readonly StudentsContext _context;
-
-        public StudentsRepository(StudentsContext context)
-        {
-            _context = context;
-        }
-        
-        public async Task<Student> GetById(Guid entityId)
-        {
-            var entity = await _context.Students
-                .Include(s => s.Enrollments)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(m => m.ExternalId == entityId);
-
-            return entity == null
-                ? null
-                : ToDomainEntity(entity);
-        }
-
-        public Task<Student[]> GetAll() => throw new NotImplementedException();
-
-        public async Task Save(Student entity)
-        {
-            var student = await _context.Students.FirstOrDefaultAsync(x => x.ExternalId == entity.EntityId);
-            if (student == null)
-            {
-                student = new Models.Student();
-
-                UpdateDataModelWithDomain(student, entity);
-
-                await _context.AddAsync(student);
-            }
-            else
-            {
-                UpdateDataModelWithDomain(student, entity);
-            }
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException exception)
-            {
-                //Log the error (uncomment ex variable name and write a log.)
-                throw new PersistenceException(
-                    "Unable to save changes. Try again, and if the problem persists, see your system administrator.",
-                    exception);
-            }
-        }
-
-        public async Task Remove(Guid entityId)
-        {
-            var student = await _context.Students.FirstOrDefaultAsync(x => x.ExternalId == entityId);
-            if (student == null)
-                throw new EntityNotFoundException(nameof(student), entityId);
-
-            _context.Students.Remove(student);
-
-            await _context.SaveChangesAsync();
-        }
+        public StudentsRepository(StudentsContext dbContext) : base(dbContext) { }
 
         public async Task<EnrollmentDateGroup[]> GetEnrollmentDateGroups()
         {
             var groups = new List<EnrollmentDateGroup>();
 
-            var conn = _context.Database.GetDbConnection();
+            var conn = DbContext.Database.GetDbConnection();
             try
             {
                 await conn.OpenAsync();
@@ -110,7 +50,7 @@ namespace ContosoUniversity.Data.Students
             return groups.ToArray();
         }
 
-        private Student ToDomainEntity(Models.Student data)
+        protected override Student ToDomainEntity(Models.Student data)
         {
             return new(
                 data.LastName,
@@ -140,8 +80,8 @@ namespace ContosoUniversity.Data.Students
                 _ => throw new ArgumentOutOfRangeException(nameof(data), data, null)
             };
         }
-
-        private void UpdateDataModelWithDomain(Models.Student model, Student entity)
+        
+        protected override void MapDomainEntityOntoDataEntity(Models.Student model, Student entity)
         {
             model.LastName = entity.LastName;
             model.FirstMidName = entity.FirstName;
