6752d82285e51064288041e17577d923b93d7c59
Alex Bogomol <alexander.bogomol@gmail.com>
2021-07-25 21:25:32 +0300

    Replace context with repository usages

--- src/ContosoUniversity/Services/Handlers/Departments/DeleteDepartmentCommandHandler.cs
+++ src/ContosoUniversity/Services/Handlers/Departments/DeleteDepartmentCommandHandler.cs
@@ -20,25 +20,27 @@ namespace ContosoUniversity.Services.Handlers.Departments
     public class DeleteDepartmentCommandHandler : AsyncRequestHandler<DeleteDepartmentCommand>
     {
         private readonly DepartmentsContext _departmentsContext;
+        private readonly IDepartmentsRepository _departmentsRepository;
         private readonly ICoursesRepository _coursesRepository;
         private readonly IMediator _mediator;
 
         public DeleteDepartmentCommandHandler(
             DepartmentsContext departmentsContext,
+            IDepartmentsRepository departmentsRepository,
             ICoursesRepository coursesRepository,
             IMediator mediator)
         {
             _departmentsContext = departmentsContext;
+            _departmentsRepository = departmentsRepository;
             _coursesRepository = coursesRepository;
             _mediator = mediator;
         }
         
         protected override async Task Handle(DeleteDepartmentCommand request, CancellationToken cancellationToken)
         {
-            var department = await _departmentsContext.Departments
-                .FirstOrDefaultAsync(x => x.ExternalId == request.Id, cancellationToken);
-            if (department == null)
-                throw new EntityNotFoundException(nameof(department), request.Id);
+            var exists = await _departmentsRepository.Exists(request.Id);
+            if (!exists)
+                throw new EntityNotFoundException(nameof(exists), request.Id);
             
             var relatedCourses = await _coursesRepository.GetByDepartmentId(request.Id);
             var relatedCoursesIds = relatedCourses.Select(x => x.EntityId).ToArray();
@@ -50,16 +52,15 @@ namespace ContosoUniversity.Services.Handlers.Departments
                 .Where(x => relatedCoursesIds.Contains(x.CourseExternalId))
                 .ToArrayAsync(cancellationToken: cancellationToken);
             _departmentsContext.CourseAssignments.RemoveRange(relatedAssignments);
-            
-            /*
-             * remove department
-             */
-            _departmentsContext.Departments.Remove(department);
 
             await _departmentsContext.SaveChangesAsync(cancellationToken);
             
+            await _departmentsRepository.Remove(request.Id);
+            
             /*
-             * remove related courses and withdraw enrolled students
+             * - remove related courses
+             * - withdraw enrolled students
+             * - remove related assignments ???
              */
             await _mediator.Publish(
                 new DepartmentDeletedNotification(request.Id, relatedCoursesIds),
