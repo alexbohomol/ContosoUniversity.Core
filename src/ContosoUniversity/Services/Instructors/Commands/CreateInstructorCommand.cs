f47260afc41e78ef996ecc59922920e83a5630bf
Alex Bogomol <alexbogomol@users.noreply.github.com>
2021-10-04 22:11:13 +0300

    Add FluentValidation for `Instructor` (#25)
    Added **FluentValidation** to validate `Instructor`
    
    - used `SetInheritanceValidator` for commands validation to include rules from respective form validators
    - implement validators for instructors

--- src/ContosoUniversity/Services/Instructors/Commands/CreateInstructorCommand.cs
+++ src/ContosoUniversity/Services/Instructors/Commands/CreateInstructorCommand.cs
@@ -1,7 +1,6 @@
 namespace ContosoUniversity.Services.Instructors.Commands
 {
     using System;
-    using System.ComponentModel.DataAnnotations;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -12,25 +11,10 @@ namespace ContosoUniversity.Services.Instructors.Commands
 
     public class CreateInstructorCommand : IRequest
     {
-        [StringLength(50, MinimumLength = 1)]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "The first character must upper case and the remaining characters must be alphabetical")]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
-
         public Guid[] SelectedCourses { get; set; }
-
-        [StringLength(50)]
-        [Display(Name = "Office Location")]
         public string Location { get; set; }
 
         public bool HasAssignedOffice => !string.IsNullOrWhiteSpace(Location);
---
+++ src/ContosoUniversity/Services/Instructors/Validators/CreateInstructorCommandValidator.cs
@@ -0,0 +1,17 @@
+namespace ContosoUniversity.Services.Instructors.Validators
+{
+    using Commands;
+
+    using FluentValidation;
+
+    public class CreateInstructorCommandValidator : AbstractValidator<CreateInstructorCommand>
+    {
+        public CreateInstructorCommandValidator()
+        {
+            RuleFor(x => x).SetInheritanceValidator(x =>
+            {
+                x.Add(new CreateInstructorFormValidator());
+            });
+        }
+    }
+}
