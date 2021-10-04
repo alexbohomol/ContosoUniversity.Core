0edd6cd5e70034beaa0620d83101a542c602ead1
Alex Bogomol <alexander.bogomol@gmail.com>
2021-10-04 22:07:00 +0300

    Add Validators for editing instructors

---
+++ src/ContosoUniversity/Services/Instructors/Validators/EditInstructorFormValidator.cs
@@ -0,0 +1,16 @@
+namespace ContosoUniversity.Services.Instructors.Validators
+{
+    using FluentValidation;
+
+    using ViewModels.Instructors;
+
+    public class EditInstructorFormValidator : AbstractValidator<EditInstructorForm>
+    {
+        public EditInstructorFormValidator()
+        {
+            RuleFor(x => x.LastName).SatisfiesLastNameRequirements();
+            RuleFor(x => x.FirstName).SatisfiesFirstNameRequirements();
+            RuleFor(x => x.Location).SatisfiesLocationRequirements();
+        }
+    }
+}
