51f3286a4281f297a2c6e7928f4067f513375edc
Alex Bogomol <alexander.bogomol@gmail.com>
2021-12-03 18:19:39 +0200

    Add migration to map from domain model

--- src/ContosoUniversity.Data.Courses/Migrations/20211203155504_MapToDomainModel.Designer.cs
+++ src/ContosoUniversity.Data.Courses/Migrations/20211203155504_MapToDomainModel.Designer.cs
@@ -4,6 +4,7 @@ using ContosoUniversity.Data.Courses;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoUniversity.Data.Courses.Migrations
 {
     [DbContext(typeof(CoursesContext))]
-    partial class CoursesContextModelSnapshot : ModelSnapshot
+    [Migration("20211203155504_MapToDomainModel")]
+    partial class MapToDomainModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
