72e299859fa237c2751e773633b41ba564dc8dd2
Alex Bogomol <alexander.bogomol@gmail.com>
2021-12-05 21:37:48 +0200

    Split base repository implementation into read and write parts

--- src/ContosoUniversity.Data/EfRepository{TDomainEntity}.cs
+++ src/ContosoUniversity.Data/EfRepository{TDomainEntity}.cs
@@ -61,3 +61,69 @@ public abstract class EfRepository<TDomainEntity>
         }
     }
 }
+
+public abstract class EfRwRepository<TDomainEntity> : IRwRepository<TDomainEntity>
+    where TDomainEntity : class, IIdentifiable<Guid>
+{
+    protected readonly DbContext DbContext;
+    protected readonly IQueryable<TDomainEntity> DbQuery;
+    protected readonly DbSet<TDomainEntity> DbSet;
+
+    protected EfRwRepository(DbContext dbContext)
+    {
+        DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        DbSet = DbContext.Set<TDomainEntity>();
+        DbQuery = DbSet;
+    }
+
+    public async Task<TDomainEntity> GetById(Guid entityId, CancellationToken cancellationToken = default)
+    {
+        return await DbQuery.FirstOrDefaultAsync(x => x.ExternalId == entityId, cancellationToken);
+    }
+
+    public async Task Save(TDomainEntity entity, CancellationToken cancellationToken = default)
+    {
+        var existing = await DbContext.FindAsync<TDomainEntity>(entity.ExternalId);
+        if (existing is null)
+            await DbSet.AddAsync(entity, cancellationToken);
+        else
+            DbSet.Update(entity);
+
+        await DbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task Remove(Guid entityId, CancellationToken cancellationToken = default)
+    {
+        var existing = await DbContext.FindAsync<TDomainEntity>(entityId);
+        if (existing is not null)
+        {
+            DbSet.Remove(existing);
+            await DbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
+
+public abstract class EfRoRepository<TDomainEntity> : IRoRepository<TDomainEntity>
+    where TDomainEntity : class, IIdentifiable<Guid>
+{
+    protected readonly DbContext DbContext;
+    protected readonly IQueryable<TDomainEntity> DbQuery;
+    protected readonly DbSet<TDomainEntity> DbSet;
+
+    protected EfRoRepository(DbContext dbContext)
+    {
+        DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        DbSet = DbContext.Set<TDomainEntity>();
+        DbQuery = DbSet;
+    }
+
+    public async Task<bool> Exists(Guid entityId, CancellationToken cancellationToken = default)
+    {
+        return await DbSet.AnyAsync(x => x.ExternalId == entityId, cancellationToken);
+    }
+
+    public async Task<TDomainEntity[]> GetAll(CancellationToken cancellationToken = default)
+    {
+        return await DbQuery.ToArrayAsync(cancellationToken);
+    }
+}
