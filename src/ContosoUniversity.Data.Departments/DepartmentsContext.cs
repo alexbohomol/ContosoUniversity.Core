8a2475bdb6dd6c055b0290545ca014a114b67e63
Alex Bogomol <alexander.bogomol@gmail.com>
2021-12-09 21:40:39 +0200

    Remove DAL models for Departments schema

--- src/ContosoUniversity.Data.Departments/DepartmentsContext.cs
+++ src/ContosoUniversity.Data.Departments/DepartmentsContext.cs
@@ -2,8 +2,6 @@
 
 using Microsoft.EntityFrameworkCore;
 
-using Models;
-
 public class DepartmentsContext : DbContext
 {
     private const string Schema = "dpt";
@@ -12,16 +10,8 @@ public class DepartmentsContext : DbContext
     {
     }
 
-    public DbSet<Department> Departments { get; set; }
-    public DbSet<Instructor> Instructors { get; set; }
-    public DbSet<CourseAssignment> CourseAssignments { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Department>().ToTable("Department", Schema);
-        modelBuilder.Entity<Instructor>().ToTable("Instructor", Schema);
-        modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment", Schema);
-        modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment", Schema)
-            .HasKey(c => new { c.CourseExternalId, c.InstructorId });
+        modelBuilder.HasDefaultSchema(Schema);
     }
 }
