897e964833774a93d23a38130fc5bfb23c529185
Alex Bogomol <alexander.bogomol@gmail.com>
2022-06-12 02:15:24 +0300

    Fixing dockerfile for MSSQL container

--- database/container/db-init.sh
+++ database/container/db-init.sh
@@ -2,8 +2,11 @@ echo "wait 25s for the SQL Server to come up"
 sleep 25s
 
 echo "running set up script"
-#run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -d master -i ../db-create.sql
+# 1st step - run the setup script to create the DB and the schema in the DB
+# TODO: create a combined script to run all steps in one shot
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -d master -i db-create.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -d master -i db-login.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -d master -i db-user.sql
 
 # 2nd step - migrate database just created
 echo "Placeholder: migrate database [NOACTION]"
