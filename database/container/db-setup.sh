c191c16b763de5a9ca6eddfb37cb6b4e19731b22
Alex Bogomol <alexander.bogomol@gmail.com>
2023-01-06 23:14:58 +0200

    Group scripts for database migration

--- database/container/db-setup.sh
+++
@@ -1,5 +0,0 @@
-echo "wait 25s for the SQL Server to come up"
-sleep 25s
-
-echo "Run the init scripts for database"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -d master -i db-init.sql
