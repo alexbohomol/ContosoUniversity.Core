1d7626464238d60127369af9f6593cc3b7553928
Alex Bohomol <alexander.bogomol@gmail.com>
2024-03-17 18:39:45 +0200

    Include `Request` into `Form`: Students

--- apps/monolith/src/ContosoUniversity.Mvc/ViewModels/Students/EditStudentForm.cs
+++ apps/monolith/src/ContosoUniversity.Mvc/ViewModels/Students/EditStudentForm.cs
@@ -1,18 +1,6 @@
 namespace ContosoUniversity.Mvc.ViewModels.Students;
 
-using System;
-
-using Application.Contracts.Repositories.ReadOnly.Projections;
-
-public class EditStudentForm : EditStudentRequest
+public record EditStudentForm
 {
-    public EditStudentForm(Student student)
-    {
-        ArgumentNullException.ThrowIfNull(student, nameof(student));
-
-        LastName = student.LastName;
-        FirstName = student.FirstName;
-        EnrollmentDate = student.EnrollmentDate;
-        ExternalId = student.ExternalId;
-    }
+    public EditStudentRequest Request { get; init; } = new();
 }
--- apps/monolith/src/ContosoUniversity.Mvc/ViewModels/Students/EditStudentRequest.cs
+++ apps/monolith/src/ContosoUniversity.Mvc/ViewModels/Students/EditStudentRequest.cs
@@ -3,18 +3,32 @@ namespace ContosoUniversity.Mvc.ViewModels.Students;
 using System;
 using System.ComponentModel.DataAnnotations;
 
-public class EditStudentRequest
+using Application.Contracts.Repositories.ReadOnly.Projections;
+
+public record EditStudentRequest
 {
+    public EditStudentRequest(Student student)
+    {
+        ArgumentNullException.ThrowIfNull(student, nameof(student));
+
+        LastName = student.LastName;
+        FirstName = student.FirstName;
+        EnrollmentDate = student.EnrollmentDate;
+        ExternalId = student.ExternalId;
+    }
+
+    public EditStudentRequest() { }
+
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [Display(Name = "Enrollment Date")]
-    public DateTime EnrollmentDate { get; set; }
+    public DateTime EnrollmentDate { get; init; }
 
     [Display(Name = "Last Name")]
-    public string LastName { get; set; }
+    public string LastName { get; init; }
 
     [Display(Name = "First Name")]
-    public string FirstName { get; set; }
+    public string FirstName { get; init; }
 
-    public Guid ExternalId { get; set; }
+    public Guid ExternalId { get; init; }
 }
