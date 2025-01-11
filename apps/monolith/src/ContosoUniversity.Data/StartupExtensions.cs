5fd38d9707a6c1ddc0194de98eb387af405028de
Alex Bohomol <alexander.bogomol@gmail.com>
2025-01-11 22:26:55 +0200

    Reduce complexity

--- apps/monolith/src/ContosoUniversity.Data/StartupExtensions.cs
+++ apps/monolith/src/ContosoUniversity.Data/StartupExtensions.cs
@@ -2,7 +2,11 @@ namespace ContosoUniversity.Data;
 
 using Connection;
 
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 public static class StartupExtensions
 {
@@ -12,4 +16,36 @@ public static class StartupExtensions
 
         services.ConfigureOptions<SqlConnectionStringBuilderSetup>();
     }
+
+    public static void AddDbContext<TDbContext>(
+        this IServiceCollection services,
+        string connectionStringName)
+        where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>((provider, options) =>
+        {
+            var connectionString = provider
+                .GetRequiredService<IConfiguration>()
+                .GetConnectionString(connectionStringName);
+
+            var defaults = provider
+                .GetRequiredService<IOptions<SqlConnectionStringBuilder>>()
+                .Value;
+
+            connectionString = connectionString.WrapWithDefaults(defaults);
+
+            options.UseSqlServer(connectionString);
+        });
+    }
+
+    private static string WrapWithDefaults(
+        this string connectionString,
+        SqlConnectionStringBuilder defaults)
+        => new SqlConnectionStringBuilder(connectionString)
+        {
+            DataSource = defaults.DataSource,
+            InitialCatalog = defaults.InitialCatalog,
+            MultipleActiveResultSets = defaults.MultipleActiveResultSets,
+            TrustServerCertificate = defaults.TrustServerCertificate
+        }.ConnectionString;
 }
