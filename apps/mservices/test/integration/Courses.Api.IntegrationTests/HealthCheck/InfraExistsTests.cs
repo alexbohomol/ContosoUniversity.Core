6c33569a90b20f919c666905b0ab117878eddea5
Alex Bohomol <alexander.bogomol@gmail.com>
2025-01-12 03:10:17 +0200

    `TestContainers` for `Courses.Api` int-tests

---
+++ apps/mservices/test/integration/Courses.Api.IntegrationTests/HealthCheck/InfraExistsTests.cs
@@ -0,0 +1,43 @@
+namespace Courses.Api.IntegrationTests.HealthCheck;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using HealthChecks.UI.Core;
+
+using Xunit;
+
+public class InfraExistsTests :
+    IClassFixture<TestsConfiguration>,
+    IClassFixture<DefaultApplicationFactory>,
+    IClassFixture<InfrastructureContext>
+{
+    private readonly HttpClient _httpClient;
+
+    public InfraExistsTests(
+        TestsConfiguration config,
+        DefaultApplicationFactory factory,
+        InfrastructureContext context)
+    {
+        factory.DataSourceSetterFunction = () => context.MsSqlDataSource;
+        factory.ClientOptions.BaseAddress = config.BaseAddressHttpsUrl;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("/health/readiness")]
+    [InlineData("/health/liveness")]
+    public async Task Health_ReturnsHealthy(string healthUrl)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(healthUrl);
+
+        response.Should().BeSuccessful();
+        response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+
+        var report = await response.Content.ReadFromJsonAsync<UIHealthReport>(JsonSerializerOptions.HealthChecks);
+
+        report.ShouldBeHealthy();
+    }
+}
