c51222fc802fe878de4994929b1231420cac9efb
Alex Bohomol <alexander.bogomol@gmail.com>
2025-01-16 19:09:23 +0200

    Fix styles

--- apps/mservices/test/integration/ContosoUniversity.Mvc.IntegrationTests/SharedTestContext.cs
+++ apps/mservices/test/integration/ContosoUniversity.Mvc.IntegrationTests/SharedTestContext.cs
@@ -35,14 +35,14 @@ public class SharedTestContext : IAsyncLifetime
                 .UsingGet())
             .RespondWith(Response.Create()
                 .WithBody("{}")
-// """
-// {
-//   "31a130fe-b396-4bb8-88d3-26fa8778b4c6": "Economics",
-//   "dab7e678-e3e7-4471-8282-96fe52e5c16f": "Engineering",
-//   "377c186a-6782-4367-9246-e5fe4195a97c": "English",
-//   "72c0804d-b208-4e67-82ba-cf54dc93dcc8": "Mathematics"
-// }
-// """)
+                // """
+                // {
+                //   "31a130fe-b396-4bb8-88d3-26fa8778b4c6": "Economics",
+                //   "dab7e678-e3e7-4471-8282-96fe52e5c16f": "Engineering",
+                //   "377c186a-6782-4367-9246-e5fe4195a97c": "English",
+                //   "72c0804d-b208-4e67-82ba-cf54dc93dcc8": "Mathematics"
+                // }
+                // """)
                 .WithHeader("content-type", "application/json; charset=utf-8")
                 .WithStatusCode(200));
 
@@ -74,48 +74,48 @@ public class SharedTestContext : IAsyncLifetime
                 .WithStatusCode(200));
 
         //         _studentsApi
-//             .Given(Request.Create()
-//                 .WithPath("/api/students/search")
-//                 .WithBodyAsJson(
-// """
-// {
-//     "SearchRequest": { "SearchString": null },
-//     "OrderRequest": { "SortOrder": null },
-//     "PageRequest": { "PageNumber": 1, "PageSize": 3 }
-// }
-// """)
-//                 .UsingPost())
-//             .RespondWith(Response.Create()
-//                 .WithBodyAsJson(
-// """
-// {
-//     "Items": [],
-//     "Info": { "PageIndex": 1, "HasPreviousPage": "false", "HasNextPage": "false" }
-// }
-// """)
-//                 .WithHeader("content-type", "application/json; charset=utf-8")
-//                 .WithStatusCode(200));
+        //             .Given(Request.Create()
+        //                 .WithPath("/api/students/search")
+        //                 .WithBodyAsJson(
+        // """
+        // {
+        //     "SearchRequest": { "SearchString": null },
+        //     "OrderRequest": { "SortOrder": null },
+        //     "PageRequest": { "PageNumber": 1, "PageSize": 3 }
+        // }
+        // """)
+        //                 .UsingPost())
+        //             .RespondWith(Response.Create()
+        //                 .WithBodyAsJson(
+        // """
+        // {
+        //     "Items": [],
+        //     "Info": { "PageIndex": 1, "HasPreviousPage": "false", "HasNextPage": "false" }
+        // }
+        // """)
+        //                 .WithHeader("content-type", "application/json; charset=utf-8")
+        //                 .WithStatusCode(200));
 
-//         _studentsApi
-//             .Given(Request.Create()
-//                 .WithPath("/api/students/search")
-//                 .WithBodyAsJson(new
-//                 {
-//                     SearchRequest = null as SearchRequest,
-//                     OrderRequest = null as OrderRequest,
-//                     PageRequest = new PageRequest(1, 3)
-//                 })
-//                 .UsingPost())
-//             .RespondWith(Response.Create()
-//                 .WithBodyAsJson(new
-// {
-//     Items = Array.Empty<>(),
-//     "Info": { "PageIndex": 1, "HasPreviousPage": "false", "HasNextPage": "false" }
-// })
-//                 .WithHeader("content-type", "application/json; charset=utf-8")
-//                 .WithStatusCode(200));
-//
-         return Task.CompletedTask;
+        //         _studentsApi
+        //             .Given(Request.Create()
+        //                 .WithPath("/api/students/search")
+        //                 .WithBodyAsJson(new
+        //                 {
+        //                     SearchRequest = null as SearchRequest,
+        //                     OrderRequest = null as OrderRequest,
+        //                     PageRequest = new PageRequest(1, 3)
+        //                 })
+        //                 .UsingPost())
+        //             .RespondWith(Response.Create()
+        //                 .WithBodyAsJson(new
+        // {
+        //     Items = Array.Empty<>(),
+        //     "Info": { "PageIndex": 1, "HasPreviousPage": "false", "HasNextPage": "false" }
+        // })
+        //                 .WithHeader("content-type", "application/json; charset=utf-8")
+        //                 .WithStatusCode(200));
+        //
+        return Task.CompletedTask;
     }
 
     public Task DisposeAsync()
