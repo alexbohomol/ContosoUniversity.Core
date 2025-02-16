fdc978f118f0f67fe07ca509bf85f7de639f8c51
Alex Bohomol <alexander.bogomol@gmail.com>
2025-02-17 00:32:08 +0200

    Auto-configure RabbitMq testing containers

--- apps/mservices/test/integration/Courses.Api.IntegrationTests/RabbitMqContext.cs
+++ apps/mservices/test/integration/Courses.Api.IntegrationTests/RabbitMqContext.cs
@@ -1,15 +1,23 @@
 namespace Courses.Api.IntegrationTests;
 
 using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+
+using IntegrationTesting.SharedKernel;
 
 using Testcontainers.RabbitMq;
 
 public class RabbitMqContext : IAsyncLifetime
 {
+    private const string RabbitmqConf = "../../../../../../../../rabbitmq.conf";
+    private const string DefinitionsJson = "../../../../../../../../definitions.json";
+
     private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
         .WithImage("rabbitmq:management")
         .WithUsername("guest")
         .WithPassword("guest")
+        .WithBindMount(RabbitmqConf.ToAbsolutePath(), "/etc/rabbitmq/rabbitmq.conf", AccessMode.ReadOnly)
+        .WithBindMount(DefinitionsJson.ToAbsolutePath(), "/etc/rabbitmq/definitions.json", AccessMode.ReadOnly)
         // .WithPortBinding(5672, 5672)
         // .WithPortBinding(15672, 15672)
         .WithWaitStrategy(
