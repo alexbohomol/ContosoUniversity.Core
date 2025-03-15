4b05cd9744ae828481ac15428a6db0f884790598
Alex Bohomol <alexander.bogomol@gmail.com>
2025-03-15 20:06:20 +0200

    Add projects for worker integration tests

---
+++ apps/mservices/test/integration/Departments.Worker.IntegrationTests/DefaultApplicationFactory.cs
@@ -0,0 +1,39 @@
+namespace Departments.Worker.IntegrationTests;
+
+using System;
+
+using Departments.Worker;
+
+using MassTransit;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+public class DefaultApplicationFactory : WebApplicationFactory<IAssemblyMarker>
+{
+    public Func<string> DataSourceSetterFunction = () => string.Empty;
+    public Func<string> RabbitMqConnectionSetterFunction;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.Configure<SqlConnectionStringBuilder>(options =>
+            {
+                options.DataSource = DataSourceSetterFunction();
+            });
+
+            if (RabbitMqConnectionSetterFunction is not null)
+            {
+                services.Configure<RabbitMqTransportOptions>(options =>
+                {
+                    var rabbitUri = new Uri(RabbitMqConnectionSetterFunction());
+                    options.Port = (ushort)rabbitUri.Port;
+                });
+            }
+        });
+    }
+}
