7f491f05a59e7cc8d6f722b4386b1490d39ea414
Alex Bohomol <alexander.bogomol@gmail.com>
2025-04-20 16:25:42 +0300

    Change outputs

--- apps/monolith/iac/envs/qa-aws-ebt/outputs.tf
+++ apps/monolith/iac/envs/qa-aws-ebt/outputs.tf
@@ -1,7 +1,14 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "beanstalk_env_url" {
+  value       = module.beanstalk.beanstalk_env_url
+  description = "Beanstalk environment URL"
 }
 
-output "subnets" {
-  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+output "beanstalk_app_cname" {
+  value       = module.beanstalk.beanstalk_app_cname
+  description = "Public friendly Beanstalk environment URL"
+}
+
+output "contoso_db_data_source" {
+  value       = module.rds.contoso_db_data_source
+  description = "Connection string for Beanstalk environment (DataSource)"
 }
