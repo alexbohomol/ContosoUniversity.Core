7b4a3af347911fe873120c7da5c59762b1625137
Alex Bohomol <alexander.bogomol@gmail.com>
2025-04-21 12:01:49 +0300

    Refactor EBT: extract `networking` module for re-use

--- apps/monolith/iac/envs/qa-aws-ebt/main.tf
+++ apps/monolith/iac/envs/qa-aws-ebt/main.tf
@@ -1,8 +1,35 @@
+module "networking" {
+  source   = "../../modules/networking"
+  app_name = var.app_name
+}
+
+resource "aws_security_group" "eb_sg" {
+  name   = "${var.app_name}-sg"
+  vpc_id = module.networking.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-sg"
+  }
+}
 
 module "rds" {
   source               = "../../modules/rds-mssql"
-  vpc_id               = aws_vpc.main.id
-  subnet_ids           = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  vpc_id               = module.networking.vpc_id
+  subnet_ids           = module.networking.subnet_ids
   eb_security_group_id = aws_security_group.eb_sg.id
   db_password          = var.db_password
   db_username          = var.db_username
@@ -12,8 +39,8 @@ module "beanstalk" {
   source                 = "../../modules/beanstalk"
   app_name               = var.app_name
   environment            = var.environment
-  vpc_id                 = aws_vpc.main.id
-  subnet_ids             = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  vpc_id                 = module.networking.vpc_id
+  subnet_ids             = module.networking.subnet_ids
   eb_security_group_id   = aws_security_group.eb_sg.id
   contoso_db_data_source = module.rds.contoso_db_data_source
 }
