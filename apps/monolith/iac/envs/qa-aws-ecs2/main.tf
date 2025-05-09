f906e37b74bdd285a9e34f500ac0174d516385f4
Alex Bohomol <alexander.bogomol@gmail.com>
2025-05-09 19:04:40 +0300

    ECS take 2: mssql

--- apps/monolith/iac/envs/qa-aws-ecs2/main.tf
+++ apps/monolith/iac/envs/qa-aws-ecs2/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 resource "aws_ecs_task_definition" "web_task" {
-  family                   = "${var.app_name}-tasks-family"
+  family                   = "${var.app_name}-web-tasks"
   cpu                      = "256"
   memory                   = "1024"
   requires_compatibilities = ["FARGATE"]
@@ -29,6 +29,41 @@ resource "aws_ecs_task_definition" "web_task" {
         { name = "ASPNETCORE_URLS", value = "http://+:80" },
         { name = "SqlConnectionStringBuilder__DataSource", value = "localhost" }
       ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "mssql_task" {
+  family                   = "${var.app_name}-mssql-tasks"
+  cpu                      = "1024"
+  memory                   = "3072"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      essential = true
+      name      = "mssql"
+      image     = "mcr.microsoft.com/mssql/server"
+      portMappings = [
+        {
+          containerPort = 1433,
+          hostPort      = 1433
+        }
+      ]
+      environment = [
+        { name = "ACCEPT_EULA", value = "Y" },
+        { name = "SA_PASSWORD", value = "<YourStrong!Passw0rd>" }
+      ]
+      healthCheck = {
+        command = [
+          "CMD-SHELL",
+          "/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P '<YourStrong!Passw0rd>' -Q \"SELECT 1\" -C"
+        ]
+        interval    = 5
+        timeout     = 5
+        retries     = 10
+        startPeriod = 5
+      }
     }
   ])
 }
