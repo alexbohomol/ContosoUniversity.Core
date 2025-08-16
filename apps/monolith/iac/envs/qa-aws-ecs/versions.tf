80945b1dbcf8efca3e1dc02513fa96862d7d7e00
Alex Bohomol <alexander.bogomol@gmail.com>
2025-08-16 03:33:12 +0300

    Employ S3 backend

--- apps/monolith/iac/envs/qa-aws-ecs/versions.tf
+++ apps/monolith/iac/envs/qa-aws-ecs/versions.tf
@@ -7,6 +7,14 @@ terraform {
       version = "5.94.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "contoso-tf-state-1ed3a58b-c137-4a46-b4a2-6e9265c0e719"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "tfstate-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
