0f150fe32c92bf91995e238e72e0c513403eb0ea
Alex Bohomol <alexander.bogomol@gmail.com>
2025-10-16 18:28:16 +0300

    Introduce ALB

--- apps/monolith/iac/envs/qa-aws-ecs/main.tf
+++ apps/monolith/iac/envs/qa-aws-ecs/main.tf
@@ -270,5 +270,46 @@ resource "aws_ecs_service" "web_service" {
     assign_public_ip = true
   }
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.web_tg.arn
+    container_name   = "web"
+    container_port   = 80
+  }
+
   depends_on = [aws_iam_role_policy_attachment.task_execution_policy]
+}
+
+# ALB and Target Group for web-service
+
+resource "aws_lb" "web_alb" {
+  name               = "${var.app_name}-alb"
+  load_balancer_type = "application"
+  security_groups    = [module.networking.sg_id]
+  subnets            = module.networking.subnet_ids
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name        = "${var.app_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.networking.vpc_id
+  target_type = "ip"
+  health_check {
+    path                = "/health/readiness"
+    interval            = 15
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
 }
