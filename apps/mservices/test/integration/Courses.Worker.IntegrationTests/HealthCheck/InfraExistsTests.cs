8a36024f28d61488ba83c26f0587afe86e1310a0
Alex Bohomol <alexander.bogomol@gmail.com>
2025-12-25 15:05:36 +0200

    Introduce `Courses.Worker.IntegrationTests` project

---
+++ apps/mservices/test/integration/Courses.Worker.IntegrationTests/HealthCheck/InfraExistsTests.cs
@@ -0,0 +1,67 @@
+namespace Courses.Worker.IntegrationTests.HealthCheck;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using HealthChecks.UI.Core;
+
+using IntegrationTesting.SharedKernel;
+
+using Xunit;
+
+public class InfraExistsTests :
+    IClassFixture<TestsConfiguration>,
+    IClassFixture<DefaultApplicationFactory>,
+    IClassFixture<InfrastructureContext>,
+    IClassFixture<RabbitMqContext>
+{
+    private readonly HttpClient _httpClient;
+
+    public InfraExistsTests(
+        TestsConfiguration config,
+        DefaultApplicationFactory factory,
+        InfrastructureContext context,
+        RabbitMqContext rabbitMqContext)
+    {
+        factory.RabbitMqConnectionSetterFunction = () => rabbitMqContext.ConnectionString;
+        factory.DataSourceSetterFunction = () => context.MsSqlDataSource;
+        factory.ClientOptions.BaseAddress = config.BaseAddressHttpsUrl;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("/health/readiness")]
+    [InlineData("/health/liveness")]
+    public async Task Health_ReturnsHealthy(string healthUrl)
+    {
+        await Task.Delay(1000);
+        HttpResponseMessage response = await _httpClient.GetAsync(healthUrl);
+
+        response.Should().BeSuccessful();
+        response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+
+        var report = await response.Content.ReadFromJsonAsync<UIHealthReport>(JsonSerializerOptionsBuilder.HealthChecks);
+
+        report.Should().NotBeNull();
+        report.Status.Should().Be(UIHealthStatus.Healthy);
+        report.Entries.Should().NotBeEmpty();
+        report.Entries.Keys.Should().BeEquivalentTo([
+            "sql-courses-writes",
+            "masstransit-bus"
+        ]);
+        report.Entries["sql-courses-writes"].Status.Should().Be(UIHealthStatus.Healthy);
+        report.Entries["sql-courses-writes"].Tags.Should().BeEquivalentTo([
+            "db",
+            "sql",
+            "courses",
+            "writes"
+        ]);
+        report.Entries["masstransit-bus"].Status.Should().Be(UIHealthStatus.Healthy);
+        report.Entries["masstransit-bus"].Tags.Should().BeEquivalentTo([
+            "ready",
+            "masstransit"
+        ]);
+    }
+}
