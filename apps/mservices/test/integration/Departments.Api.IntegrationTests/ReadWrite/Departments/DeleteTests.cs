6026006aa9b9c15adaaf08b48ea38c954584c6d0
Alex Bohomol <alexander.bogomol@gmail.com>
2025-12-26 23:51:28 +0200

    Enable and extend skipped test

--- apps/mservices/test/integration/Departments.Api.IntegrationTests/ReadWrite/Departments/DeleteTests.cs
+++ apps/mservices/test/integration/Departments.Api.IntegrationTests/ReadWrite/Departments/DeleteTests.cs
@@ -2,40 +2,56 @@ namespace Departments.Api.IntegrationTests.ReadWrite.Departments;
 
 using System.Net;
 
+using ContosoUniversity.Messaging.Contracts;
+
 using FluentAssertions;
 
 using IntegrationTesting.SharedKernel;
 
+using Models;
+
 public class DeleteTests :
     IClassFixture<TestsConfiguration>,
     IClassFixture<DefaultApplicationFactory<IAssemblyMarker>>,
-    IClassFixture<MsSqlContext>
+    IClassFixture<MsSqlContext>,
+    IClassFixture<RabbitMqContext>
 {
     private readonly HttpClient _httpClient;
+    private readonly RabbitMqClient _rabbitMqClient;
 
     public DeleteTests(
         TestsConfiguration config,
         DefaultApplicationFactory<IAssemblyMarker> factory,
-        MsSqlContext msSqlContext)
+        MsSqlContext msSqlContext,
+        RabbitMqContext rabbitMqContext)
     {
+        factory.RabbitMqConnectionSetterFunction = () => rabbitMqContext.ConnectionString;
+        _rabbitMqClient = new RabbitMqClient(rabbitMqContext.ConnectionString);
         factory.DataSourceSetterFunction = () => msSqlContext.MsSqlDataSource;
         factory.ClientOptions.BaseAddress = config.BaseAddressHttpsUrl;
         _httpClient = factory.CreateClient();
     }
 
-    [Fact(Skip = "TODO: Enable/update after implementing delete flow")]
+    [Fact]
     public async Task DepartmentExists_ReturnsNoContent()
     {
         // Arrange
-        (_, Uri location) = await _httpClient.CreateDepartment(Requests.Create.Department.Valid);
+        (CreateDepartmentResponse created, Uri location) = await _httpClient.CreateDepartment(Requests.Create.Department.Valid);
 
         // Act
         var response = await _httpClient.DeleteAsync(location, default);
         var content = await response.Content.ReadAsStringAsync();
 
-        // Assert
+        // Assert api response
+
         response.Should().BeSuccessful();
         response.StatusCode.Should().Be(HttpStatusCode.NoContent);
         content.Should().BeEmpty();
+
+        // Assert message bus event
+
+        var dptEvent = await _rabbitMqClient.TryConsumeAsync<DepartmentDeletedEvent>("department-deleted-event-handler-courses");
+        dptEvent.Should().NotBeNull();
+        dptEvent.DepartmentId.Should().Be(created.ExternalId);
     }
 }
