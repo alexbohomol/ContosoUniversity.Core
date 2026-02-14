90c42e822dbaba05ab6e90bb9a6edecaab526795
Alex Bohomol <alexander.bogomol@gmail.com>
2026-02-14 22:46:26 +0200

    Add Route 53 DNS configuration for application URL

--- apps/monolith/iac/envs/qa-aws-ecs/main.tf
+++ apps/monolith/iac/envs/qa-aws-ecs/main.tf
@@ -385,3 +385,17 @@ resource "aws_lb_listener" "web_listener" {
     target_group_arn = aws_lb_target_group.web_tg.arn
   }
 }
+
+# Route 53 DNS Configuration
+
+resource "aws_route53_record" "web_alb_alias" {
+  zone_id = var.route53_zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.web_alb.dns_name
+    zone_id                = aws_lb.web_alb.zone_id
+    evaluate_target_health = true
+  }
+}
--- apps/monolith/iac/envs/qa-aws-ecs/outputs.tf
+++ apps/monolith/iac/envs/qa-aws-ecs/outputs.tf
@@ -9,3 +9,18 @@ output "sg_id" {
 output "subnet_ids" {
   value = module.networking.subnet_ids
 }
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.web_alb.dns_name
+}
+
+output "custom_domain_name" {
+  description = "Custom domain name pointing to the ALB"
+  value       = var.domain_name
+}
+
+output "application_url" {
+  description = "URL to access the application"
+  value       = "http://${var.domain_name}"
+}
