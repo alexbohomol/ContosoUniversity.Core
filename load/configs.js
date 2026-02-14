44bab0e2221701d593f5b7de8ee4dd4a4d6d9076
Alex Bohomol <alexander.bogomol@gmail.com>
2026-02-14 23:44:29 +0200

    Update load tests to point to QA environment

--- load/configs.js
+++ load/configs.js
@@ -1,4 +1,4 @@
-const appRoot = __ENV.BASE_URL || 'http://contoso-mnlth-alb-1720592476.eu-central-1.elb.amazonaws.com';
+const appRoot = __ENV.BASE_URL || 'http://qa-aws-ecs.mnlth.university.contoso.name';
 
 export default {
     rootUrl: appRoot,
