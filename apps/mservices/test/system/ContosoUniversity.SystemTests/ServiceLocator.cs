6fd00b56b8db30890cae3a3fc5b64b85bd8cfdd1
Alex Bohomol <alexander.bogomol@gmail.com>
2026-05-10 13:37:58 +0300

    CONTOSO: Refine `CA1859` - rename field

--- apps/mservices/test/system/ContosoUniversity.SystemTests/ServiceLocator.cs
+++ apps/mservices/test/system/ContosoUniversity.SystemTests/ServiceLocator.cs
@@ -7,12 +7,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceLocator
 {
-    static ServiceLocator()
-    {
-        ServiceProvider = BuildServiceProvider();
-    }
+    static ServiceLocator() => _serviceProvider = BuildServiceProvider();
 
-    private static readonly IServiceProvider ServiceProvider;
+    private static readonly IServiceProvider _serviceProvider;
 
     private static ServiceProvider BuildServiceProvider()
     {
@@ -27,8 +24,5 @@ public static class ServiceLocator
         return services.BuildServiceProvider();
     }
 
-    public static T GetRequiredService<T>()
-    {
-        return ServiceProvider.GetRequiredService<T>();
-    }
+    public static T GetRequiredService<T>() => _serviceProvider.GetRequiredService<T>();
 }
