65880a4c066c7ccb36ba65cdb5ae6d55781280af
Alex Bohomol <alexander.bogomol@gmail.com>
2026-09-20 15:58:05 +0300

    MSVC: copy tests from monolith

---
+++ apps/mservices/test/system/ContosoUniversity.SystemTests/DepartmentsController/CreateDepartmentRequest.cs
@@ -0,0 +1,42 @@
+namespace ContosoUniversity.SystemTests.DepartmentsController;
+
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+public record CreateDepartmentRequest
+{
+    public string Name { get; init; }
+    public decimal Budget { get; init; }
+    public DateTime StartDate { get; init; }
+    public string AdministratorName { get; init; }
+
+    public static readonly CreateDepartmentRequest Valid = new()
+    {
+        Name = "Informatics",
+        Budget = 1000000.00m,
+        StartDate = new DateTime(2021, 9, 1),
+        AdministratorName = "Zheng, Roger"
+    };
+
+    public static IEnumerable<TestCaseData> ValidInstructorVariants =>
+    [
+        new TestCaseData(Valid with { AdministratorName = null })
+            .SetName("Without_Administrator"),
+        new TestCaseData(Valid)
+            .SetName("With_Administrator")
+    ];
+
+    public static IEnumerable<TestCaseData> Invalids =>
+    [
+        new TestCaseData(
+            Valid with { Name = "XY" },
+            "'Name' must be between 3 and 50 characters. You entered 2 characters.")
+            .SetName("Name_is_too_short"),
+        new TestCaseData(
+            Valid with { Name = new string('X', 51) },
+            "'Name' must be between 3 and 50 characters. You entered 51 characters.")
+            .SetName("Name_is_too_long"),
+    ];
+}
--- apps/mservices/test/system/ContosoUniversity.SystemTests/SutUrls.cs
+++ apps/mservices/test/system/ContosoUniversity.SystemTests/SutUrls.cs
@@ -9,6 +9,10 @@ public class SutUrls(IConfiguration configuration)
     public string CoursesDeletePage => $"{BaseAddress}/Courses/Delete";
     public string CoursesEditPage => $"{BaseAddress}/Courses/Edit";
     public string CoursesListPage => $"{BaseAddress}/Courses";
+    public string DepartmentsCreatePage => $"{BaseAddress}/Departments/Create";
+    public string DepartmentsDeletePage => $"{BaseAddress}/Departments/Delete";
+    public string DepartmentsEditPage => $"{BaseAddress}/Departments/Edit";
+    public string DepartmentsListPage => $"{BaseAddress}/Departments";
     public string StudentsCreatePage => $"{BaseAddress}/Students/Create";
     public string StudentsDeletePage => $"{BaseAddress}/Students/Delete";
     public string StudentsEditPage => $"{BaseAddress}/Students/Edit";
